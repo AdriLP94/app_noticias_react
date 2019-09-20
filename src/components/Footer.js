8a1ec4d9cad106813636ad500777861199dd0338
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-09-20 19:52:13 +0200

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <nav className="nav-wrapper light-blue darken-3 footer-copyright">
+            <p className="center-align">Pagina realizada por Adrián López Palmero</p>
+        </nav>
+    );
+};
+
+export default Footer;
