3167e94b9369357768ed67d673aec2ff6d07eb25
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-09-20 19:52:21 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const Header = ({titulo}) => {
     return (
         <nav className="nav-wrapper light-blue darken-3">
-            <a className="brand-logo center" href={window.location.href}>{titulo}</a>
+            <span className="brand-logo center">{titulo}</span>
         </nav>
     );
 };
+Header.propTypes = {
+    titulo : PropTypes.string.isRequired
+}
 
 export default Header;
