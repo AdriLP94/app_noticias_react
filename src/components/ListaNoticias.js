bc95ae67eff8fe5ad51faebb25799ab6d13dc79f
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-09-20 19:52:25 +0200

    Update ListaNoticias.js

--- src/components/ListaNoticias.js
+++ src/components/ListaNoticias.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import Noticia from "./Noticia"
+import Noticia from "./Noticia";
+import PropTypes from "prop-types";
 
 const ListNoticias = ({noticias}) => {
+
+    const listado = (noticias.length && Array.isArray(noticias)) ?  
+        noticias.map(noticia =>(
+            <Noticia
+                key = {noticia.url}
+                noticia = {noticia}
+            />
+        ))
+        :<p> No hay noticias de este tipo, porfavor cambie de país o categoría </p>;
+
     return (
         <div className="row">
-            {noticias.map(noticia =>(
-                <Noticia
-                    key = {noticia.url}
-                    noticia = {noticia}
-                />
-            ))}
+            {listado}
         </div>
     );
 };
+
+ListNoticias.propTypes = {
+    noticias : PropTypes.array.isRequired
+}
 
 export default ListNoticias;
